2d41ca9cbc7609987804bfc1f856d351fd88b711
Lucas <lucas@agtlucas.com>
2013-12-25 12:11:51 -0200

    Update grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -294,7 +294,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: '../',
+          remote: 'git@github.com:AgtLucas/project-hawk.git',
           branch: 'gh-pages',
           commit: true,
           push: true
